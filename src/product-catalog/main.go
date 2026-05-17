aa6cb8dec78b1ffb858f2e4cc72169fc657728e6
gopi-techs <gopiaj97@gmail.com>
2026-05-17 13:03:16 +0530

    update code32

--- src/product-catalog/main.go
+++ src/product-catalog/main.go
@@ -366,7 +366,7 @@ func (p *productCatalog) GetProduct(ctx context.Context, req *pb.GetProductReque
 		span.SetStatus(otelcodes.Error, msg)
 		span.AddEvent(msg)
 		return nil, status.Errorf(codes.Internal, "internal error: %v", err)
-
+	}
 	found, err := getProductFromDB(ctx, req.Id)
 	if err != nil {
 		msg := fmt.Sprintf("Product Not Found: %s", req.Id)
